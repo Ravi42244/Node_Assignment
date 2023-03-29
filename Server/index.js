f9e162ee953e62e4af567af8d7884af83f3cc182
Ravi42244 <ravi42244@gmail.com>
2023-03-29 12:45:52 +0530

    frontend first stage complete

--- Server/index.js
+++ Server/index.js
@@ -27,7 +27,7 @@ app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({policy : "cross-origin"}));
 
 app.use(cors());
-const port = 3001
+const port = process.env.PORT || 3001
 
 app.use(bodyParser.urlencoded({extended: true}))
 
