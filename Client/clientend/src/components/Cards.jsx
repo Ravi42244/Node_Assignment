3a716d8312552069d1815fb0c5f78447ac843ea1
Ravi42244 <ravi42244@gmail.com>
2023-03-30 02:19:33 +0530

    added spinner and resolve key error

--- Client/clientend/src/components/Cards.jsx
+++ Client/clientend/src/components/Cards.jsx
@@ -1,65 +1,57 @@
-import React,{useEffect, useState} from 'react';
-import Card from '../components/Card.jsx';
+import React, { useEffect, useState } from "react";
+import Card from "../components/Card.jsx";
 
- const Cards = (props)=>{
- 
-    const [reportTO_display,setreportTO_display]=useState(false)
-    const [ids,setids]=useState(props.data.reportTo)
-    
-    
-    useEffect(()=>{
+const Cards = (props) => {
+  const [reportTO_display, setreportTO_display] = useState(false);
+  const [ids, setids] = useState(props.data.reportTo);
 
-        setids(props.data.reportTo)
-    },[props.data])
-    
-    
-    
-    useEffect(()=>{
-        if(props.data.reportTo){
-            
+  useEffect(() => {
+    setids(props.data.reportTo);
+  }, [props.data]);
 
-            setreportTO_display(true)
-        }else(
+  useEffect(() => {
+    if (props.data.reportTo) {
+      setreportTO_display(true);
+    } else setreportTO_display(false);
+  }, [props.data]);
 
-            setreportTO_display(false)
-                    )
-        
-        
-    },[props.data])
-
-    return (
-        <div className='container text-center'>
-        <div className='row my-5'>
-            <div className='col'>
-       {(props.data.firstName) ? (<Card firstName={props.data.firstName} lastName={props.data.lastName} email={props.data.email} role={props.data.role} />) : <>User Not Found!</>}
-       </div>
-       </div>
-      
-
-{reportTO_display === true ? (<div className='row'>{ids.map((data,i)=> (
-
-
-
-
-
-<div className='col'>
-         <Card firstName={data.firstName} lastName={data.lastName} email={data.email} role={data.role} key={i}/> 
-         </div>
-         
-
-
-
-
-
-))}</div>) : <></>}
-
-
-
-
+  return (
+    <div className="container text-center">
+      <div className="row my-5">
+        <div className="col">
+          {props.data.firstName ? (
+            <Card
+              firstName={props.data.firstName}
+              lastName={props.data.lastName}
+              email={props.data.email}
+              role={props.data.role}
+            />
+          ) : (
+            <>User Not Found!</>
+          )}
         </div>
-    )
+      </div>
 
-
-}
+      {reportTO_display === true ? (
+        <div className="row">
+          {ids.map((data, i) => {
+            return (
+              <div className="col" key={i}>
+                <Card
+                  firstName={data.firstName}
+                  lastName={data.lastName}
+                  email={data.email}
+                  role={data.role}
+                />
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
 
 export default Cards;
--- Client/clientend/src/components/FormwithFormik.jsx
+++ Client/clientend/src/components/FormwithFormik.jsx
@@ -1,87 +1,111 @@
-import React,{useState, useEffect} from 'react'
-import {Formik,Form,Field } from 'formik'
-
+import React, { useState, useEffect } from "react";
+import { Formik, Form, Field } from "formik";
 
 import axios from "axios";
 
-
-
 function FormwithFormik(props) {
- const [Paste, setPaste] = useState(false)
- const [copiedEmail, setcopiedEmail] = useState({email:''})
-    const formTitle = {
-        fontSize: "50px",
-        fontStyle: "bold",
-      };
-      const initialValues = {
-        email : ''
-    }
+  const [Paste, setPaste] = useState(false);
+  const [copiedEmail, setcopiedEmail] = useState({ email: "" });
+  const [loading, setloading] = useState(false);
 
+  const formTitle = {
+    fontSize: "50px",
+    fontStyle: "bold",
+  };
+  const initialValues = {
+    email: "",
+  };
 
-
-
-const onSubmit = async (values) => {
-   let  email = values.email;
-  await  axios
+  const onSubmit = async (values) => {
+    let email = values.email;
+    await axios
       .get(`${window.env.BACKEND_URL}${email}`)
       .then((res) => {
+        setloading(false);
         props.getData(res.data);
       })
       .catch((err) => {
+        setloading(false);
         props.getData(err);
         console.error(err);
       });
-      
   };
 
-
   useEffect(() => {
-    console.log(copiedEmail)
-    if(Paste){
-    onSubmit(copiedEmail)
-    setPaste(false)
+    if (Paste) {
+      onSubmit(copiedEmail);
+      setPaste(false);
     }
-    
-  }, [copiedEmail])
+  }, [copiedEmail]);
   return (
     <div className="container">
-    <Formik initialValues={Paste ? initialValues : copiedEmail} className="container my-5 text-center" onSubmit={onSubmit} enableReinitialize>
-    <Form >
-      <div className="mb-3 text-center">
-        <label className="form-label " style={formTitle}>
-          Employee Name
-        </label>
-        <Field
-         name='email'
-          className="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-          required={true}
-          />
-      </div>
-      <div className="row">
-        <button type="submit" className="btn btn-primary container col mx-3">
-          Search
-        </button>
-        <button id="pasteandsearch"
-          type="button"
-          onClick={async () => {
-           
-            const copiedData=  await navigator.clipboard.readText();
-            
-            setcopiedEmail({email:copiedData})
-          setPaste(true)
-                
+      <Formik
+        initialValues={Paste ? initialValues : copiedEmail}
+        className="container my-5 text-center"
+        onSubmit={onSubmit}
+        enableReinitialize
+      >
+        <Form>
+          <div className="mb-3 text-center">
+            <label className="form-label " style={formTitle}>
+              Employee Name
+            </label>
+            <Field
+              name="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              required={true}
+            />
+          </div>
+          <div className="row">
+            <button
+              type="submit"
+              className="btn btn-primary container col mx-3"
+              onClick={() => {
+                setloading(true);
+              }}
+            >
+              Search
+            </button>
+
+            <button
+              id="pasteandsearch"
+              type="button"
+              onClick={async () => {
+                setloading(true);
+
+                const copiedData = await navigator.clipboard.readText();
+
+                setcopiedEmail({ email: copiedData });
+              }}
+              className="btn btn-primary container col mx-3"
+            >
+              Paste & Search
+            </button>
+          </div>
+        </Form>
+      </Formik>
+
+      {loading ? (
+        <div
+          className="d-flex justify-content-center"
+          style={{
+            marginTop: "300px",
+            marginLeft: "633px",
+            zIndex: 10,
+            position: "absolute",
           }}
-          className="btn btn-primary container col mx-3"
         >
-          Paste & Search
-        </button>
-      </div>
-    </Form>
-  </Formik>
-  </div>
-  )
+          <div className="spinner-border text-primary" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 }
 
-export default FormwithFormik
+export default FormwithFormik;
